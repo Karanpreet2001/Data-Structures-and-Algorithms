791613669824bc268127b3be026e4d8b141cac1a
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-07-04 23:57:03 -0700

    Two sum problem

--- TwoSum.java
+++ TwoSum.java
@@ -1,2 +1,26 @@
+import java.util.HashMap;
 
-class   
+public class TwoSum{
+
+
+
+
+// HashMap    
+    public static int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer,Integer>  map = new HashMap<>();
+        
+        int[] result = new int[2];
+        
+        for(int i =0; i<nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                result[1]= i;
+                result[0] = map.get(target-nums[i]);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+        
+    }
+}
